2264b91513399bedc35421ccd1568f0976f3c2a8
Malte Bär <maltebaer@posteo.de>
2023-12-11 23:17:37 +0100

    moves data to 11ty

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,11 +7,20 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
+const sortByOrder = (values) => {
+    let vals = [...values];     // this *seems* to prevent collection mutation...
+    return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+};
+const getLocaleDateString = (date) => {
+    if (!date) {
+        return 'ongoing';
+    }
+    return new Date(date).toLocaleDateString();
+};
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/css/**/*.css");
-
     eleventyConfig.addTemplateFormats('css');
-
     eleventyConfig.addExtension('css', {
         outputFileExtension: 'css',
         compile: async (content, path) => {
@@ -31,6 +40,9 @@ module.exports = function (eleventyConfig) {
             };
         }
     });
+
+    eleventyConfig.addFilter("sortByOrder", sortByOrder);
+    eleventyConfig.addFilter("getLocaleDateString", getLocaleDateString);
 
     // Return your Object options:
     return {
