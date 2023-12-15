567949fc1045d2f3157dc4976c253a87c31415c0
Malte Bär <maltebaer@posteo.de>
2023-12-15 23:07:41 +0100

    adds tooltips for control elements

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,11 @@
 import Alpine from 'alpinejs'
+import Tooltip from "@ryangjchandler/alpine-tooltip";
+
+Alpine.plugin(
+    Tooltip.defaultProps({
+        theme: 'material',
+    })
+);
 
 window.Alpine = Alpine
-
 Alpine.start()
